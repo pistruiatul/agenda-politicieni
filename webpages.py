91a72970e0c90147c0f347a0e3aa1aa93cea0a62
Alex Morega <alex@grep.ro>
2012-01-20 23:16:25 +0200

    json download of whole database

--- webpages.py
+++ webpages.py
@@ -9,3 +9,8 @@ webpages = flask.Blueprint('webpages', __name__)
 def home():
     return flask.render_template('homepage.html',
                                  persons=database.get_persons())
+
+
+@webpages.route('/download.json')
+def download():
+    return flask.jsonify(database.get_persons())
