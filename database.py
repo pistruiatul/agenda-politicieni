fbe3b0ee3ffcd0380120a704a1e0c99d35d1d3e8
Alex Morega <alex@grep.ro>
2012-01-20 22:49:38 +0200

    load fixture into database

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
-import py
+import os.path
+from flask import json
 from flaskext.sqlalchemy import SQLAlchemy
 
 
@@ -10,3 +11,24 @@ class Contact(db.Model):
     person = db.Column(db.Integer)
     name = db.Column(db.Text())
     value = db.Column(db.Text())
+
+
+def import_fixture(flush=True):
+    data_path = os.path.join(os.path.dirname(__file__), 'data')
+    fixture_path = os.path.join(data_path, 'fixture.json')
+
+    if flush:
+        db.drop_all()
+        db.create_all()
+
+    with open(fixture_path, 'rb') as f:
+        fixture = json.load(f)
+
+    for person in fixture:
+        person_id = person.pop('id')
+        for key in person:
+            record = Contact(person=person_id, name=key)
+            record.value = person[key]
+            db.session.add(record)
+
+    db.session.commit()
