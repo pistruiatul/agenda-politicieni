4009e4d5d9005d2e1b7272c6f97e6ee808c726fb
Alex Morega <alex@grep.ro>
2012-01-22 01:09:52 +0200

    python 2.6 compatibility

--- webpages.py
+++ webpages.py
@@ -26,7 +26,7 @@ def download():
 @webpages.route('/person/<int:person_id>')
 def person(person_id):
     person = database.Person.query.get_or_404(person_id)
-    prop_map = {p.name: p.value for p in person.properties.all()}
+    prop_map = dict((p.name, p.value) for p in person.properties.all())
     suggestions = [{'name': s.name, 'value': s.value, 'date': s.date}
                    for s in person.suggestions.all()]
     return flask.render_template('person.html',
