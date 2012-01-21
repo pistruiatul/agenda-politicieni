b3340a32e7f6b68966aec6e8e9ee135f5b0b72af
Alex Morega <alex@grep.ro>
2012-01-21 17:34:32 +0200

    move property definitions to json file

--- webpages.py
+++ webpages.py
@@ -1,5 +1,11 @@
+import os.path
 import flask
 import database
+
+
+_data_dir = os.path.join(os.path.dirname(__file__), 'data')
+with open(os.path.join(_data_dir, 'prop_defs.json'), 'rb') as f:
+    prop_defs = flask.json.load(f)
 
 
 webpages = flask.Blueprint('webpages', __name__)
@@ -24,7 +30,4 @@ def suggest(person_id):
 
 def init_app(app):
     app.register_blueprint(webpages)
-    app.jinja_env.globals['known_names'] = {
-        'email': "Email",
-        'phone': "Telefon",
-    }
+    app.jinja_env.globals['known_names'] = prop_defs
